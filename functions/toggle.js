f669543ae899eccd668f414cc6b2c06032591c83
nihakue <walden.gabrielw@gmail.com>
2019-01-04 00:09:52 +0000

    remove functions

--- functions/toggle.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async function(event, context) {
-    const {identity, user} = context.clientContext;
-    return {
-        statusCode: 200,
-        body: `Hello, ${JSON.stringify(user)}`
-    }
-}
